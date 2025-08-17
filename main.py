d2bc7ccabe40ee816538371e699d56ebdd41f69e
LbsTempest <l1641101764@gmail.com>
2025-08-18 00:31:55 +0800

    refactor yml to yaml

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.workflow_runner import WorkflowRunner
 
 if __name__ == "__main__":
-    runner = WorkflowRunner(config_path="config.yml")
+    runner = WorkflowRunner(config_path="config.yaml")
     runner.run()
